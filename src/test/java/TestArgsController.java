7c12d0e27decd46a5a16c64203fa5451d5131efd
cbourgeois <bourgeois.car@northeastern.edu>
2026-07-01 13:54:54 -0400

    Task 2: TestArgsController testDefaultValues() #1

---
+++ src/test/java/TestArgsController.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import student.controller.ArgsController;
+import student.model.formatters.Formats;
+
+public class TestArgsController {
+    /**
+     * test that ArgsController returns the correct default values.
+     * new String[]{} creates new, empty array of Strings.
+     */
+    @Test
+    public void testDefaultValues() {
+        ArgsController controller = new ArgsController(new String[]{});
+        assertEquals(Formats.PRETTY, controller.getFormat());
+        assertEquals(System.out, controller.getOutput());
+        assertEquals("all", controller.getHostname());
+    }
+}
