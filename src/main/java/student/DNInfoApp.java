16e9af3f54d963878635ca7154e703ddb840b336
cbourgeois <bourgeois.car@northeastern.edu>
2026-07-02 16:37:58 -0400

    Task 2: DNInfoApp implemented main() based on current stage of project

--- src/main/java/student/DNInfoApp.java
+++ src/main/java/student/DNInfoApp.java
@@ -1,6 +1,10 @@
 package student;
 
 
+import student.controller.ArgsController;
+import student.model.DomainNameModel;
+import java.util.List;
+import student.model.DomainNameModel.DNRecord;
 /**
  * Main driver for the program.
  * 
@@ -20,6 +24,22 @@ public final class DNInfoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        // pass args array into ArgsController
+        ArgsController controller = new ArgsController(args);
+        // check for help request, print msg, exit
+        if (controller.isHelpRequest()) {
+            System.out.println(controller.getHelp());
+            return;
+        }
+        // get instance from file path in controller. store result as 'model'.
+        DomainNameModel model = DomainNameModel.getInstance(controller.getDatabase());
+        List<DNRecord> records;
+        if (controller.getHostname().equals("all")) {
+            records = model.getRecords();
+        } else {
+            records = List.of(model.getRecord(controller.getHostname()));
+        }
+        DomainNameModel.writeRecords(records, controller.getFormat(), controller.getOutput());
     }
+
 }
