52551184367af43db4c8122015f90d2dd3333915
cbourgeois <bourgeois.car@northeastern.edu>
2026-07-07 14:13:43 -0400

    Task 2: DomainNameModelImpl getRecord() & saveRecord()

--- src/main/java/student/model/DomainNameModelImpl.java
+++ src/main/java/student/model/DomainNameModelImpl.java
@@ -1,8 +1,14 @@
 package student.model;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import student.model.formatters.DomainXmlWrapper;
+import student.model.net.ApiResponse;
+import student.model.net.NetUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -36,6 +42,7 @@ public class DomainNameModelImpl implements DomainNameModel {
     }
 
     /**
+     * gets records as a list.
      * make sure records are immutable / unmodifiable.
      * @return records list.
      */
@@ -44,8 +51,52 @@ public class DomainNameModelImpl implements DomainNameModel {
         return Collections.unmodifiableList(records);
     }
 
+    /**
+     * gets single record by hostname.
+     * @param hostname the hostname to look up.
+     * @return the record.
+     */
     @Override
     public DNRecord getRecord(String hostname) {
-        throw new UnsupportedOperationException("Not implemented yet");
+       for (DNRecord record : records) {
+           if (record.hostname().equals(hostname)) {
+               return record;
+           }
+       }
+       try {
+           // take hostname turn into ip address.
+           String ip = NetUtils.lookUpIp(hostname);
+           // take ip turn into raw XML.
+           InputStream stream = NetUtils.getIpDetails(ip);
+           // create Jackson mapper.
+           XmlMapper mapper = new XmlMapper();
+           // read XML from stream, call setters on ApiResponse for each field.
+           ApiResponse response = mapper.readValue(stream, ApiResponse.class);
+           // pull each field out of ApiResponse, pass to DNRecord constructor in order.
+           // hostname comes from method because ApiResponse doesn't know hostname.
+           DNRecord newRecord = new DNRecord(hostname, response.getIp(), response.getCity(), response.getRegion(),
+                   response.getCountry(), response.getPostal(), response.getLatitude(), response.getLongitude());
+           records.add(newRecord);
+           saveRecord();
+           return newRecord;
+       } catch (Exception error) {
+           error.printStackTrace();
+           return null;
+       }
+    }
+
+    /**
+     * helper method to save records.
+     * serialize current records into XML.
+     */
+    private void saveRecord() {
+        try {
+            XmlMapper mapper = new XmlMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            DomainXmlWrapper wrapper = new DomainXmlWrapper(records);
+            mapper.writeValue(new File(database), wrapper);
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
     }
 }
